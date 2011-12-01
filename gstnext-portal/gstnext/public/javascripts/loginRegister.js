4e31c60bf5f15fcfd4346c3ca0f1cfbbe83b0880
root <root@ip-10-190-206-230.ec2.internal>
2011-12-01 14:10:22 -0500

    stuff

--- gstnext-portal/gstnext/public/javascripts/loginRegister.js
+++ gstnext-portal/gstnext/public/javascripts/loginRegister.js
@@ -38,7 +38,8 @@ function submitRegister(formId)
 			contentType: 'application/json; charset=utf-8',
 			data: formData,
 			success: function(data){
-				alert("success");
+				alert(data.id);
+				alert(getCookie('session'));
 			}
 		});
 	}
@@ -80,4 +81,17 @@ function validateEmail(email) {
     var re = 
     	/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-} 
+}
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') 
+			c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) 
+			return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
