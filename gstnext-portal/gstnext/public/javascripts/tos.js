bbbb11620160da1ded4ae3e593ac72d0d7e6060c
root <root@ip-10-190-206-230.ec2.internal>
2011-12-02 14:06:45 -0500

    tos javascript

--- gstnext-portal/gstnext/public/javascripts/tos.js
+++ gstnext-portal/gstnext/public/javascripts/tos.js
@@ -1,5 +1,16 @@
+//var couch = 'http://ec2-67-202-6-195.compute-1.amazonaws.com/couch/';
+//var couch = '/couch';
+var couch = '/node';
+
 $(document).ready(function(){
 	$('#accept-tos').click(function(){
-		alert('hi');
+		//alert(getCookie('id'));
+		$.ajax({
+			type: 'POST',
+			url: couch + '/acceptTOS/' + getCookie('id'),
+			success: function(data){
+				alert('success!');
+			}
+		});
 	});
 });
