ff8b2d3b13b97e9ad804e9417678437ed94416d5
selee <selee@ign.com>
2011-12-01 18:28:21 -0800

    move cookie methods to main

--- gstnext-portal/gstnext/public/javascripts/loginRegister.js
+++ gstnext-portal/gstnext/public/javascripts/loginRegister.js
@@ -112,23 +112,3 @@ function validateEmail(email) {
     return re.test(email);
 }
 
-function getCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') 
-			c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) 
-			return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
-
-function setCookie(c_name,value,exdays)
-{
-	var exdate=new Date();
-	exdate.setDate(exdate.getDate() + exdays);
-	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString()) + "; path=/";
-	document.cookie=c_name + "=" + c_value;
-}
--- gstnext-portal/gstnext/public/javascripts/main.js
+++ gstnext-portal/gstnext/public/javascripts/main.js
@@ -29,3 +29,25 @@ function addMenuOption(label, url, last) {
 	html += '"><a href="' + url + '">' + label + '</a></div>';
 	$('#banner-buttons').append(html);
 }
+
+
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') 
+			c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) 
+			return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function setCookie(c_name,value,exdays)
+{
+	var exdate=new Date();
+	exdate.setDate(exdate.getDate() + exdays);
+	var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString()) + "; path=/";
+	document.cookie=c_name + "=" + c_value;
+}
